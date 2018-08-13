ceeadffa876a07ed662d55e109a8dcf906306118
Jaga Apple <jagaapple@uniboar.com>
2018-08-13 21:14:34 +0900

    [Add] Implement to generate a React element from raster images
    - Implement to generateReact element from raster images
    - Implement to import a React element and path using `import {} from “”;` syntax

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="node" />
 import { loader } from "webpack";
 export declare const raw: boolean;
-export default function (this: loader.LoaderContext, source: Buffer): string;
+export default function (this: loader.LoaderContext, source: Buffer): void;
